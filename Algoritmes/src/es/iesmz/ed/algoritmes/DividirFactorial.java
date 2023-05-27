8737eb9b18b8aa8df6147b494974e2c06b71a470
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-27 15:50:44 +0200

    Realización de test DividirFactorial

--- Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorial.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorial.java
@@ -1,21 +1,21 @@
 package es.iesmz.ed.algoritmes;
 
 public class DividirFactorial {
-    int numerador;
-    int denominador;
+    long numerador;
+    long denominador;
 
-    public DividirFactorial(int numerador, int denominador) {
+    public DividirFactorial(long numerador, long denominador) {
         this.numerador = numerador;
         this.denominador = denominador;
     }
-    public int divisio(int num1, int num2) {
+    public static int divisio(long num1, long num2) {
         long factorial1 = calcularFactorial(num1);
         long factorial2 = calcularFactorial(num2);
 
         return (int) (factorial1 / factorial2);
     }
 
-    public static long calcularFactorial(int num) {
+    public static long calcularFactorial(long num) {
         if (num == 0 || num == 1) {
             return 1;
         }
--- Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorialTest.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorialTest.java
@@ -1,7 +1,55 @@
 package es.iesmz.ed.algoritmes;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DividirFactorialTest {
+    @Test
+    public void test1(){
+        long resultadoReal = DividirFactorial.divisio(4,6);
+        long resultadoEsperado = (long) 0.03333333333333333;
 
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
+
+    @Test
+    public void test2(){
+        long resultadoReal = DividirFactorial.divisio(0,50);
+        long resultadoEsperado = (long) 0.02;
+
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
+
+    @Test
+    public void test3(){
+        long resultadoReal = DividirFactorial.divisio(0,0);
+        long resultadoEsperado =1;
+
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
+
+    @Test
+    public void test4(){
+        long resultadoReal = DividirFactorial.divisio(10,7);
+        long resultadoEsperado = 720;
+
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
+
+    @Test
+    public void test5(){
+        long resultadoReal = DividirFactorial.divisio(1,1);
+        long resultadoEsperado = 1;
+
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
+
+    @Test
+    public void test6(){
+        long resultadoReal = DividirFactorial.divisio(9,7);
+        long resultadoEsperado = 72;
+
+        assertEquals(resultadoEsperado, resultadoReal,0.1);
+    }
 }
