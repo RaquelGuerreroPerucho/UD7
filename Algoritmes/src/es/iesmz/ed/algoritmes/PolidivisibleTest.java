44bbae335bb8cce0cd92fc36f238c4a619116cee
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-06-03 00:39:18 +0200

    Commit test Polidivisible

--- Algoritmes/src/es/iesmz/ed/algoritmes/PolidivisibleTest.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/PolidivisibleTest.java
@@ -17,4 +17,45 @@ class PolidivisibleTest {
         assertEquals(resultadoEsperado, resultadoReal);
     }
 
+    @Test
+    public void test2() {
+        Polidivisible p1 = new Polidivisible(102);
+
+        boolean resultadoReal = p1.esPolidivisible();
+        boolean resultadoEsperado = true;
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test3() {
+        Polidivisible p1 = new Polidivisible(9876);
+
+        boolean resultadoReal = p1.esPolidivisible();
+        boolean resultadoEsperado = true;
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test4() {
+        Polidivisible p1 = new Polidivisible(7589);
+
+        boolean resultadoReal = p1.esPolidivisible();
+        boolean resultadoEsperado = false;
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test5() {
+        Polidivisible p1 = new Polidivisible(10009);
+
+        boolean resultadoReal = p1.esPolidivisible();
+        boolean resultadoEsperado = false;
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+
 }
